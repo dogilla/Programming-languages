28ae612b61386d2fd433ae194ef9c84bdf5c03a2
dogilla <19thmagm@ciencias.unam.mx>
2020-05-23 18:49:32 -0500

    Practica 5

--- practica5/desugar.rkt
+++ practica5/desugar.rkt
@@ -1,17 +1,19 @@
 #lang plai
 (require (file "./grammars.rkt"))
-;;(require (file "./parser.rkt"))
+(require (file "./parser.rkt"))
 
+
+(define (caseif lista)
+  (let ([x (car lista)])
+    (cond
+    [(condition? (car lista)) (if (condition-test-expr (car lista)) (condition-then-expr (car lista)) (caseif (cdr lista)))]
+    [(else-cond? (car lista)) (else-cond-else-expr (car lista))])))
 
 ;; Función que toma una expresión con azúcar sintáctica
 ;; SCFWBAE y elimina el azúcar sintáctica, tansformándola
 ;; en una expresión del tipo CFWBAE; formando el árbol de
 ;; sintáxis abstracta correspondiente a la expresión recibida.
 ;; desugar SCFWBAE-> CFWBAE
-
-
-
-
 (define (desugar expr)
   (type-case SCFWBAE expr
     [idS (i) (id i)]
@@ -19,9 +21,10 @@
     [boolS (b) (bool b)]
     [iFS (c t e) (if (desugar c) (desugar t) (desugar e))]
     [opS (p arg) (op p (map desugar arg))]
+    [condS (l) (caseif l)]
     [appS (fun-expr args) (app (desugar fun-expr) (foldr (λ (v l) (cons (desugar v) l)) '() args))]
     [funS (f body) (fun f (desugar body))]
-    [withS (f body) (app (fun (map (λ (v) (binding-name v)) f) (desugar body)) (map desugar (map (λ (v) (binding-value v)) f)))]
+    [withS (f body) (app (fun (map (λ (v) (binding-id v)) f) (desugar body)) (map desugar (map (λ (v) (binding-value v)) f)))]
     [withS* (f body) (cond
                        [(= 1 (length f)) (desugar(withS f body))]
                        [else (desugar (withS (list (car f)) (withS* (cdr f) body)))])]))
--- practica5/parser.rkt
+++ practica5/parser.rkt
@@ -1,6 +1,24 @@
 #lang plai
 (require (file "./grammars.rkt"))
 
+(define (simbolo s)
+  (cond
+    [(equal? s 'true) (bool #t)]
+    [(equal? s 'false) (bool #f)]
+    [else (idS s)]))
+
+
+(define (saca par)
+  (if (equal? (car par) 'else)
+      (else-cond (parse (second par)))
+      (condition (parse (first par)) (parse (second par)))))
+
+(define (opera expr)
+  (match expr ['+ +]['- -]['* *]['/ /] ['expt (λ elem (foldr expt (car elem) (cdr elem)))]
+                                        ['modulo modulo]['add1 add1] ['sub1 sub1] [else (error "NO")]))
+(define (esOperador? expr)
+  (match expr ['+ #t]['- #t]['* #t]['/ #t] ['expt #t] ['modulo #t]['add1 #t] ['sub1 #t] [else #f]))
+
 ;; Toma una lista de números, symbolos o listas
 ;; y la traduce a un árbol de sintaxis abstracta CFWBAE
 ;; A::=<number>
@@ -8,4 +26,24 @@
 ;;    | listof(A)
 ;; parse: A -> SCFWBAE
 ;; parse: s-expression -> SCFWBAE
-(define (parse sexp) ...)
+(define (parse sexp)
+  (match sexp
+    [(? symbol?)
+     (simbolo sexp)]
+    [(? boolean?) (boolS sexp)]
+    [(? number?)
+     (numS sexp)]
+    [(list 'with l body)
+     (withS (map (lambda (x) (binding (car x) (parse (second x)) )) l) (parse body))]
+    [(list 'with* l body)
+     (withS* (map (lambda (x) (binding (car x) (parse (second x)) )) l) (parse body))]
+    [(list 'if con else then)
+     (iFS (parse con) (parse else) (parse then))]
+    [(list 'fun l body)
+     (funS l (parse body))]
+    [(list 'cond casos)
+     (condS (foldr (lambda (v l) (cons (saca v) l)) '() casos))]
+    [(list 'app f args)
+     (appS (parse f) (map parse args))]
+    [(cons x xs)
+     [(esOperador? x) (op (opera (first sexp))  (map (lambda (x) (parse x)) (cdr sexp)))]]))
